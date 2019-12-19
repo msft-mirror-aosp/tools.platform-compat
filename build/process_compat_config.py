bionic-linux/android_tools_platform_compat

Strip meta-data from compat_config.xml.
Rework process_compat_config.py to make it easier to extend, and with
proper argument parsing. When writing config bound for the device, we strip
any child elements of <compat-change> leaving just the attributes in place.
This allows the annotation processor to add meta-data that is not needed on
device. We also keep support for writing the full config, including
metadata, for use by later build steps.

Add some tests while we're here. Follow up CLs will add the metadata
to the output from the annotation processor.

Test: atest process-compat-config-test
Change-Id: I2fb80adacaee2e3584faae581cbc0416b90615ed

build/process_compat_config.py
@@ -19,6 +19,7 @@ Extracts compat_config.xml from built jar files and merges them into a single
 XML file.
 """
 
+import argparse
 import sys
 import xml.etree.ElementTree as ET
 from zipfile import ZipFile
@@ -35,18 +36,63 @@ def extract_compat_config(jarfile):
                 with jar.open(info.filename, 'r') as xml:
                     yield xml
 
-def merge_compat_config_xml(jarfile):
-    tree = ET.ElementTree()
-    tree._setroot(ET.Element('config'))
-    for xmlFile in extract_compat_config(jarfile):
+class ConfigMerger(object):
+
+    def __init__(self):
+        self.tree = ET.ElementTree()
+        self.tree._setroot(ET.Element("config"))
+
+    def merge(self, xmlFile):
         xml = ET.parse(xmlFile)
         for child in xml.getroot():
-            tree.getroot().append(child)
-    tree.write("/dev/stdout", encoding='utf-8', xml_declaration=True)
+            self.tree.getroot().append(child)
 
+    def write(self, filename):
+        self.tree.write(filename, encoding='utf-8', xml_declaration=True)
+
+    def write_device_config(self, filename):
+        self.strip_config_for_device().write(filename, encoding='utf-8', xml_declaration=True)
+
+    def strip_config_for_device(self):
+        new_tree = ET.ElementTree()
+        new_tree._setroot(ET.Element("config"))
+        for change in self.tree.getroot():
+            new_change = ET.Element("compat-change")
+            new_change.attrib = change.attrib.copy()
+            new_tree.getroot().append(new_change)
+        return new_tree
 
 def main(argv):
-    merge_compat_config_xml(argv[1])
+    parser = argparse.ArgumentParser(
+        description="Processes compat config XML files")
+    parser.add_argument("--jar", type=argparse.FileType('rb'), action='append',
+        help="Specifies a jar file to extract compat_config.xml from.")
+    parser.add_argument("--xml", type=argparse.FileType('rb'), action='append',
+        help="Specifies an xml file to read compat_config from.")
+    parser.add_argument("--device-config", dest="device_config", type=argparse.FileType('wb'),
+        help="Specify where to write config for embedding on the device to. "
+        "Meta data not needed on the devivce is stripped form this.")
+    parser.add_argument("--merged-config", dest="merged_config", type=argparse.FileType('wb'),
+        help="Specify where to write merged config to. This will include metadata.")
+
+    args = parser.parse_args()
+
+    c = ConfigMerger()
+    if args.jar:
+        for jar in args.jar:
+            for xml in extract_compat_config(jar):
+                c.merge(xml)
+    if args.xml:
+        for xml in args.xml:
+            c.merge(xml)
+
+    if args.device_config:
+        c.write_device_config(args.device_config)
+
+    if args.merged_config:
+        c.write(args.merged_config)
+
+
 
 if __name__ == "__main__":
     main(sys.argv)

msft-mirror-aosp/tools.platform-compat

build/process_compat_config.py
@@ -19,6 +19,7 @@ Extracts compat_config.xml from built jar files and merges them into a single
 XML file.
 """
 
+import argparse
 import sys
 import xml.etree.ElementTree as ET
 from zipfile import ZipFile
@@ -35,18 +36,63 @@ def extract_compat_config(jarfile):
                 with jar.open(info.filename, 'r') as xml:
                     yield xml
 
-def merge_compat_config_xml(jarfile):
-    tree = ET.ElementTree()
-    tree._setroot(ET.Element('config'))
-    for xmlFile in extract_compat_config(jarfile):
+class ConfigMerger(object):
+
+    def __init__(self):
+        self.tree = ET.ElementTree()
+        self.tree._setroot(ET.Element("config"))
+
+    def merge(self, xmlFile):
         xml = ET.parse(xmlFile)
         for child in xml.getroot():
-            tree.getroot().append(child)
-    tree.write("/dev/stdout", encoding='utf-8', xml_declaration=True)
+            self.tree.getroot().append(child)
 
+    def write(self, filename):
+        self.tree.write(filename, encoding='utf-8', xml_declaration=True)
+
+    def write_device_config(self, filename):
+        self.strip_config_for_device().write(filename, encoding='utf-8', xml_declaration=True)
+
+    def strip_config_for_device(self):
+        new_tree = ET.ElementTree()
+        new_tree._setroot(ET.Element("config"))
+        for change in self.tree.getroot():
+            new_change = ET.Element("compat-change")
+            new_change.attrib = change.attrib.copy()
+            new_tree.getroot().append(new_change)
+        return new_tree
 
 def main(argv):
-    merge_compat_config_xml(argv[1])
+    parser = argparse.ArgumentParser(
+        description="Processes compat config XML files")
+    parser.add_argument("--jar", type=argparse.FileType('rb'), action='append',
+        help="Specifies a jar file to extract compat_config.xml from.")
+    parser.add_argument("--xml", type=argparse.FileType('rb'), action='append',
+        help="Specifies an xml file to read compat_config from.")
+    parser.add_argument("--device-config", dest="device_config", type=argparse.FileType('wb'),
+        help="Specify where to write config for embedding on the device to. "
+        "Meta data not needed on the devivce is stripped from this.")
+    parser.add_argument("--merged-config", dest="merged_config", type=argparse.FileType('wb'),
+        help="Specify where to write merged config to. This will include metadata.")
+
+    args = parser.parse_args()
+
+    config = ConfigMerger()
+    if args.jar:
+        for jar in args.jar:
+            for xml in extract_compat_config(jar):
+                config.merge(xml)
+    if args.xml:
+        for xml in args.xml:
+            config.merge(xml)
+
+    if args.device_config:
+        config.write_device_config(args.device_config)
+
+    if args.merged_config:
+        config.write(args.merged_config)
+
+
 
 if __name__ == "__main__":
     main(sys.argv)